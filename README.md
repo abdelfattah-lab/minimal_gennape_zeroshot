4ead2b62346c7b26bcf90a6a9c702e49b7229abc
Yash Akhauri <ya255@sablab-gpu-12.ece.cornell.edu>
2024-03-22 13:07:02 -0400

    ags

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 To reproduce average correlation results, simply run python ensembled.py
 
 This simply averages the score of two FLAN variants, delivering SoTA Zero-Shot Spearman-Rho on NASBench-201.
+
+
+SpearmanR for nb201_sorted_results_adj_gin_774.pkl: 0.7783529591076018
+SpearmanR for nb201_sorted_results_adj_gin_804.pkl: 0.8041116857249022
+SpearmanR correlation: 0.8494461571019523
--- ensembled.py
+++ ensembled.py
@@ -14,25 +14,12 @@ def ensemble_pred_scores_and_calculate_kendall_tau(filenames, weights=None):
             true_scores, pred_scores = zip(*data)
             true_scores = np.array(true_scores)
             pred_scores = np.array(pred_scores)
-            
-            # if true_scores_reference is not None:
-            #     import pdb; pdb.set_trace()
             if true_scores_reference is None:
                 true_scores_reference = true_scores
-            # elif not np.array_equal(true_scores_reference, true_scores):
-            #     raise ValueError("True scores are not the same across all files.")
-            
             all_pred_scores.append(pred_scores)
-
-            # Compute and print the Kendall tau for the current file
             file_kendall_tau = spearmanr(true_scores, pred_scores).correlation
-            print(f"Kendall tau for {filename}: {file_kendall_tau}")
-    
-    
-    # Stack the pred_scores vertically for easier manipulation
+            print(f"SpearmanR for {filename}: {file_kendall_tau}")
     all_pred_scores = np.vstack(all_pred_scores)
-    
-    # Compute weighted average of pred_scores if weights are given, else compute simple average
     if weights:
         if len(weights) != len(all_pred_scores):
             raise ValueError("Number of weights must match the number of pred_scores arrays.")
@@ -40,11 +27,11 @@ def ensemble_pred_scores_and_calculate_kendall_tau(filenames, weights=None):
     else:
         ensembled_pred_scores = np.mean(all_pred_scores, axis=0)
     
-    # Calculate and return the Kendall tau correlation
+    # Calculate and return the SpearmanR correlation
     return spearmanr(true_scores_reference, ensembled_pred_scores).correlation
 
 
 filenames = ["nb201_sorted_results_adj_gin_774.pkl", "nb201_sorted_results_adj_gin_804.pkl"]
 kendall_tau = ensemble_pred_scores_and_calculate_kendall_tau(filenames)
-print(f"Kendall tau correlation: {kendall_tau}")
+print(f"SpearmanR correlation: {kendall_tau}")
 
